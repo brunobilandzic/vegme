524daf26dfb57b27368d38f206689a498e57bcdf
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2022-05-22 11:58:34 +0200

    Finish certificates...

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+import axios from "axios";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 import NavBar from "./Navigation/NavBar";
 import Router from "./Routes/Router";
 
 function App() {
+  const [response, setResponse] = useState();
+  const testCert = async (e) => {
+    const response = await axios.get("https://localhost:5000");
+    console.log(response.data);
+    setResponse(response.data);
+  };
   return (
     <>
+      {JSON.stringify(response)}
+      <Button onClick={testCert}>Test me</Button>
       <Router navbar={<NavBar />} />
     </>
   );
