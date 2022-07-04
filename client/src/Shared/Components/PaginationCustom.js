b9c55914e5a06a059e4ea0de8f0615f65b327c3d
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2022-07-04 11:09:56 +0200

    Pagination css

--- client/src/Shared/Components/PaginationCustom.js
+++ client/src/Shared/Components/PaginationCustom.js
@@ -1,92 +1,104 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { PageItem, Pagination } from "react-bootstrap";
 import propTypes from "prop-types";
 import { connect } from "react-redux";
-
-import {
-  updatePageNumber,
-  updatePageSize,
-  updateTotalItems,
-  updateTotalPages,
-} from "../Redux/pagination/paginationActions.js";
-
+import { updatePageNumber } from "../Redux/pagination/paginationActions.js";
+import paginationStyles from "./pagination.module.css";
 function PaginationCustom(props) {
-  const {
-    meals,
-    orders,
-    loadItems,
-    updatePageNumber,
-    updateTotalItems,
-    updateTotalPages,
-    updatePageSize,
-    type
-  } = props;
+  const { meals, orders, loadItems, updatePageNumber, type } = props;
 
-  const {
-    pageNumber,
-    pageSize,
-    totalPages,
-    totalItems
-  } = type == "meals" ? meals : type == "orders" ? orders : {
-    pageNumber: 1,
-    pageSize: 5,
-    totalPages: null,
-    totalItems: null
-  } 
-
-
+  const { pageNumber, pageSize, totalPages } =
+    type == "meals"
+      ? meals
+      : type == "orders"
+      ? orders
+      : {
+          pageNumber: 1,
+          pageSize: 5,
+          totalPages: null,
+        };
 
   const getPageNumbers = () => {
     let pageItems = [];
-    for (let i = 0; i < totalPages; i++) {
-      pageItems.push(
-        <PageItem
-          onClick={() => {
-            updatePageNumber(type, i+1)
-            loadItems(i + 1, pageSize)
+    const needDots = totalPages > 5 ? true : false;
+    if (needDots) {
+      if (pageNumber > 3) pageItems.push(<PageItem disabled>{"..."}</PageItem>);
+      for (
+        let i = pageNumber - 2;
+        i <= pageNumber + 2 && i <= totalPages;
+        i++
+      ) {
+        if (i <= 0) continue;
+        pageItems.push(
+          <PageItem
+            onClick={() => {
+              updatePageNumber(type, i);
+              loadItems(i, pageSize);
             }}
-          active={i === pageNumber - 1}
-          key={i}
-        >
-          {i + 1}
-        </PageItem>
-      );
+            active={i === pageNumber}
+            key={i}
+          >
+            {i}
+          </PageItem>
+        );
+      }
+      if (pageNumber < totalPages - 2)
+        pageItems.push(<PageItem disabled>{"..."}</PageItem>);
+    } else {
+      for (let i = 1; i <= totalPages; i++) {
+        pageItems.push(
+          <PageItem
+            onClick={() => {
+              updatePageNumber(type, i);
+              loadItems(i, pageSize);
+            }}
+            active={i === pageNumber}
+            key={i}
+          >
+            {i}
+          </PageItem>
+        );
+      }
     }
     return pageItems;
   };
   const handleFirst = () => {
     updatePageNumber(type, 1);
-    loadItems(1, pageSize)
+    loadItems(1, pageSize);
   };
   const handlePrevious = () => {
     updatePageNumber(type, pageNumber - 1);
-    loadItems(pageNumber -1, pageSize)
+    loadItems(pageNumber - 1, pageSize);
   };
 
   const handleNext = () => {
     updatePageNumber(type, pageNumber + 1);
-    loadItems(pageNumber + 1, pageSize)
+    loadItems(pageNumber + 1, pageSize);
   };
   const handleLast = () => {
     updatePageNumber(type, totalPages);
-    loadItems(totalPages, pageSize)
+    loadItems(totalPages, pageSize);
   };
   return (
     <div>
-      <Pagination>
-        <PageItem disabled={pageNumber === 1} onClick={handleFirst}>
-          {"<<"}
-        </PageItem>
-        <PageItem disabled={pageNumber === 1} onClick={handlePrevious}>
-          {"<"}
-        </PageItem>
+      <Pagination className={paginationStyles.container}>
+        <div className={paginationStyles.items_on_beginning}>
+          <PageItem disabled={pageNumber === 1} onClick={handleFirst}>
+            {"<<"}
+          </PageItem>
+          <PageItem disabled={pageNumber === 1} onClick={handlePrevious}>
+            {"<"}
+          </PageItem>
+        </div>
         {getPageNumbers()}
-        <PageItem disabled={pageNumber === totalPages} onClick={handleNext}>
-          {">"}
-        </PageItem>
-        <PageItem disabled={pageNumber === totalPages} onClick={handleLast}>
-          {">>"}
-        </PageItem>
+        <div className={paginationStyles.items_on_end}>
+          <PageItem disabled={pageNumber === totalPages} onClick={handleNext}>
+            {">"}
+          </PageItem>
+          <PageItem disabled={pageNumber === totalPages} onClick={handleLast}>
+            {">>"}
+          </PageItem>
+        </div>
       </Pagination>
     </div>
   );
@@ -94,17 +106,14 @@ function PaginationCustom(props) {
 
 PaginationCustom.propTypes = {
   orders: propTypes.object,
-  meals: propTypes.object
+  meals: propTypes.object,
 };
 
 const mapStateToProps = (state) => ({
   orders: state.pagination.orders,
-  meals: state.pagination.meals
+  meals: state.pagination.meals,
 });
 
 export default connect(mapStateToProps, {
   updatePageNumber,
-  updatePageSize,
-  updateTotalItems,
-  updateTotalPages,
 })(PaginationCustom);
