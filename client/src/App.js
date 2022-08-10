84ab7aa33d5a3ab31da31114850a85fa63ebfcd5
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2022-08-10 15:45:01 +0200

    Socket skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,15 @@ import Router from "./Routes/Router";
 import propTypes from "prop-types";
 import { loadUser } from "./Redux/auth/authActions.js";
 import { useEffect } from "react";
+import SocketComponent from "./Socket/SocketComponent";
+
 function App({ loadUser }) {
-  useEffect(() => {loadUser()}, []);
+  useEffect(() => {
+    loadUser();
+  }, []);
   return (
     <>
+      <SocketComponent />
       <Router navbar={<NavBar />} />
     </>
   );
@@ -20,4 +25,4 @@ App.propTypes = {
 
 const mapStateToProps = (state) => ({});
 
-export default connect(mapStateToProps, {loadUser})(App);
+export default connect(mapStateToProps, { loadUser })(App);
